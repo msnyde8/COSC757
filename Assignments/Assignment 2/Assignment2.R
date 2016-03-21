57d80c6b72775592c81e89b63c4b453a4aeb9e5a
msnyde8 <msnyde8@students.towson.edu>
2016-03-20 21:53:15 -0400

    First draft of Assignment 2 paper, screenshots, and updates to R file

--- Assignments/Assignment 2/Assignment2.R
+++ Assignments/Assignment 2/Assignment2.R
@@ -1,34 +1,36 @@
-#### Input data set iris into Data Frame "irisData"
-irisData <- read.csv(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment2/UCI data/iris.data.txt", header = FALSE, sep = ",", stringsAsFactors = TRUE, col.names = c("sLength", "sWidth", "pLength", "pWidth", "class") )
-irisData[1:20,]
+#### Input data set bakabce scale into Data Frame "balanceScaleData"
+balanceScaleData <- read.csv(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment2/UCI data/balance-scale.data", header = FALSE, sep = ",", stringsAsFactors = TRUE, col.names = c("class", "lWeight", "lDistance", "rWeight", "rDistance"))
+balanceScaleData[1:20,]
 
 ## Decision Tree Classification
 # install.packages("rpart")
 # library(rpart)
 set.seed(1234)
-ind <- sample(2, nrow(irisData), replace=TRUE,
+ind <- sample(2, nrow(balanceScaleData), replace=TRUE,
             prob=c(0.7,0.3))
-trainData <- irisData[ind==1,]
-testData <- irisData[ind==2,]
-iris_rpart <- rpart(class ~ sLength + sWidth + pLength + pWidth, data = trainData)
-printcp(iris_rpart)
-plotcp(iris_rpart)
-plot(iris_rpart)
-text(iris_rpart, use.n=TRUE)
-iris_pred <- predict(iris_rpart, newData = testData)
-iris_pred
+trainData <- balanceScaleData[ind==1,]
+testData <- balanceScaleData[ind==2,]
+trainData$balanceData <- (trainData$rWeight * trainData$rDistance) - (trainData$lWeight * trainData$lDistance)
+testData$balanceData <- (testData$rWeight * testData$rDistance) - (testData$lWeight * testData$lDistance)
+balanceScale_rpart <- rpart(class ~ balanceData, data = trainData)
+printcp(balanceScale_rpart)
+plotcp(balanceScale_rpart)
+plot(balanceScale_rpart)
+text(balanceScale_rpart, use.n=TRUE)
+balanceScale_pred <- predict(balanceScale_rpart, newData = testData)
+balanceScale_pred
 
 ## Naive Bayes Classification
 # install.packages("e1071")
 # library(class)
 # library(e1071)
-classifier <- naiveBayes(class ~ sLength + sWidth + pLength + pWidth, data = trainData)
+classifier <- naiveBayes(class ~ balanceData, data = trainData)
 pred <- predict(classifier, testData[,-5])
 table(pred,testData$class)
 
 ## Random Forest?
 # install.packages("randomForest")
 # library(randomForest)
-fit <- randomForest(class ~ sLength + sWidth + pLength + pWidth, data = trainData)
+fit <- randomForest(class ~ balanceData, data = trainData)
 print(fit)
 importance(fit)
