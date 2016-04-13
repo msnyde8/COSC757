9a062c991d31b54b0a03cf2e7abf79961f5c4f54
msnyde8 <msnyde8@students.towson.edu>
2016-04-13 06:34:05 -0400

    Updated R file for assignment 3

--- Assignments/Assignment 3/Assignment3.R
+++ Assignments/Assignment 3/Assignment3.R
@@ -8,45 +8,46 @@ library(cluster)
 library(fpc)
 
 ## Data setup
-irisData <- read.csv(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment2/UCI data/try1/iris.data.txt", header = FALSE, sep = ",", stringsAsFactors = TRUE, col.names = c("sLength", "sWidth", "pLength", "pWidth", "class") )
-plot(irisData)
-newData <- cbind(irisData$pWidth, irisData$sWidth)
+#irisData <- read.csv(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment2/UCI data/try1/iris.data.txt", header = FALSE, sep = ",", stringsAsFactors = TRUE, col.names = c("sLength", "sWidth", "pLength", "pWidth", "class") )
+#plot(irisData)
+paperData <- read.csv(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment3/UCI data/[UCI] AAAI-13 Accepted Papers - Papers.csv", header = FALSE, stringsAsFactors = TRUE, col.names = c("title", "keywords", "topics", "hlKeywords", "abstract") )
+plot(paperData)
+newData <- cbind(paperData$topics, paperData$hlKeywords)
 plot(newData)
 
-
 ## Density-based approach: DBSCAN
-#Run dbscan with eps = .05 and MinPts = 5
-dbr <- dbscan(newData, eps=.05, MinPts=5)
+#Run dbscan with eps = 5 and MinPts = 5
+dbr <- dbscan(newData, eps=5, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
-#Run dbscan with eps = .10 and MinPts = 5
-dbr <- dbscan(newData, eps=.10, MinPts=5)
+#Run dbscan with eps = 10 and MinPts = 5
+dbr <- dbscan(newData, eps=10, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
-#Run dbscan with eps = .20 and MinPts = 5 
-dbr <- dbscan(newData, eps=.20, MinPts=5)
+#Run dbscan with eps = 15 and MinPts = 5 
+dbr <- dbscan(newData, eps=15, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
-#Run dbscan with eps = .50 and MinPts = 5 
-dbr <- dbscan(newData, eps=.50, MinPts=5)
+#Run dbscan with eps = 11 and MinPts = 5 
+dbr <- dbscan(newData, eps=11, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
-#Run dbscan with eps = .65 and MinPts = 5 
-dbr <- dbscan(newData, eps=.65, MinPts=5)
+#Run dbscan with eps = 12 and MinPts = 5 
+dbr <- dbscan(newData, eps=12, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
-#Run dbscan with eps = .60 and MinPts = 5 
-dbr <- dbscan(newData, eps=.60, MinPts=5)
+#Run dbscan with eps = 11.5 and MinPts = 5 
+dbr <- dbscan(newData, eps=11.5, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
-#Run dbscan with eps = .61 and MinPts = 5 
-dbr <- dbscan(newData, eps=.61, MinPts=5)
+#Run dbscan with eps = 11.1 and MinPts = 5 
+dbr <- dbscan(newData, eps=11.1, MinPts=5)
 str(dbr)
 plot(newData, col=dbr$cluster+1L)
 
@@ -59,62 +60,69 @@ plot(sil)
 
 ## Partitioning approach: K-means
 plot(newData)
+myData <- newData
+wss<-(nrow(myData)-1)*sum(apply(myData,2,var))
+  for (i in 2:15) wss[i] <- sum(kmeans(myData,
+                                       centers=i)$withinss)
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups")
 
-#run k-means with k = 2
-km2 <- kmeans(newData, centers = 2) 
-km2
-#visualize results colored by cluster
-plot(newData, col=km2$cluster)
-#plot cluster centers
-points(km2$centers,pch='x',cex=1.5)
 
-#run k-means with k = 3
-km3 <- kmeans(newData, centers = 3) 
-km3
+#run k-means with k = 8
+km8 <- kmeans(newData, centers = 8) 
+km8
 #visualize results colored by cluster
-plot(newData, col=km3$cluster)
+plot(newData, col=km8$cluster)
 #plot cluster centers
-points(km3$centers,pch='x',cex=1.5)
+points(km8$centers,pch='x',cex=1.5)
+
+#run k-means with k = 10
+km10 <- kmeans(newData, centers = 10)
+km10
+#visualize results colored by cluster
+plot(newData, col=km10$cluster)
+#plot cluster centers
+points(km10$centers,pch='x',cex=1.5)
 
 #evaluating k-means in R
 #create distance matrix for cluster.stats
 distm <- dist(newData)
-#calculate cluster statistics for km2
-cstatskm2 = cluster.stats(distm,km2$cluster)
-#calculate cluster statistics for km3
-cstatskm3 = cluster.stats(distm,km3$cluster) 
+#calculate cluster statistics for km8
+cstatskm8 = cluster.stats(distm,km8$cluster)
+#calculate cluster statistics for km10
+cstatskm10 = cluster.stats(distm,km10$cluster) 
 #evaluate between and within cluster distances
-cstatskm2$average.between
-cstatskm2$average.within
-cstatskm3$average.between
-cstatskm3$average.within
+cstatskm8$average.between
+cstatskm8$average.within
+cstatskm10$average.between
+cstatskm10$average.within
 
 ## Partitioning approach: K-Medoids
 distm <- dist(newData)
 
-#run PAM with k = 2
-pam2 <- pam(newData, 2)
+#run PAM with k = 8
+pam8 <- pam(newData, 8)
 #visualize results colored by cluster
-plot(newData, col=pam2$cluster)
+plot(newData, col=pam8$cluster)
 #plot cluster centers
-points(pam2$medoids,pch='O',cex=1.5)
+points(pam8$medoids,pch='O',cex=1.5)
 
-#run PAM with k = 3
-pam3 <- pam(newData, 3)
+#run PAM with k = 10
+pam10 <- pam(newData, 10)
 #visualize results colored by cluster
-plot(newData, col=pam3$cluster)
+plot(newData, col=pam10$cluster)
 #plot cluster centers
-points(pam3$medoids,pch='O',cex=1.5)
+points(pam10$medoids,pch='O',cex=1.5)
 
 #evaluate PAM
-cstatspam2 <- cluster.stats(distm,pam2$cluster)
-cstatspam3 <- cluster.stats(distm,pam3$cluster)
+cstatspam8 <- cluster.stats(distm,pam8$cluster)
+cstatspam10 <- cluster.stats(distm,pam10$cluster)
 
 #evaluate between and within cluster distances
-cstatskm2$average.between
-cstatskm2$average.within
-cstatskm3$average.between
-cstatskm3$average.within
+cstatspam8$average.between
+cstatspam8$average.within
+cstatspam10$average.between
+cstatspam10$average.within
 
 
 ## Hierarchical approach: AGNES - Single linkage
