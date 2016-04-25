88548d156a2d636cfe204509e5bab15bed4a347d
msnyde8 <msnyde8@students.towson.edu>
2016-04-25 00:01:07 -0400

    Files for Assignment 4

---
+++ Assignments/Assignment 4/Assignment4.R
@@ -0,0 +1,113 @@
+#### Input data set plants scale into Data Frame "plantsData"
+votingData <- read.csv(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment4/UCI data/house-votes-84.data.txt", header = FALSE, sep = ",", stringsAsFactors = FALSE)
+votingData.ID<-seq.int(nrow(votingData))
+votingData[1:20,]
+votingData$V1 <- as.factor(votingData$V1)
+votingData$V2[votingData$V2=="y"]<-"2y"
+votingData$V2[votingData$V2=="n"]<-"2n"
+votingData$V2[votingData$V2=="?"]<-""
+votingData$V2 <- as.factor(votingData$V2)
+votingData$V3[votingData$V3=="y"]<-"3y"
+votingData$V3[votingData$V3=="n"]<-"3n"
+votingData$V3[votingData$V3=="?"]<-""
+votingData$V3 <- as.factor(votingData$V3)
+votingData$V4[votingData$V4=="y"]<-"4y"
+votingData$V4[votingData$V4=="n"]<-"4n"
+votingData$V4[votingData$V4=="?"]<-""
+votingData$V4 <- as.factor(votingData$V4)
+votingData$V5[votingData$V5=="y"]<-"5y"
+votingData$V5[votingData$V5=="n"]<-"5n"
+votingData$V5[votingData$V5=="?"]<-""
+votingData$V5 <- as.factor(votingData$V5)
+votingData$V6[votingData$V6=="y"]<-"6y"
+votingData$V6[votingData$V6=="n"]<-"6n"
+votingData$V6[votingData$V6=="?"]<-""
+votingData$V6 <- as.factor(votingData$V6)
+votingData$V7[votingData$V7=="y"]<-"7y"
+votingData$V7[votingData$V7=="n"]<-"7n"
+votingData$V7[votingData$V7=="?"]<-""
+votingData$V7 <- as.factor(votingData$V2)
+votingData$V8[votingData$V8=="y"]<-"8y"
+votingData$V8[votingData$V8=="n"]<-"8n"
+votingData$V8[votingData$V8=="?"]<-""
+votingData$V8 <- as.factor(votingData$V2)
+votingData$V9[votingData$V9=="y"]<-"9y"
+votingData$V9[votingData$V9=="n"]<-"9n"
+votingData$V9[votingData$V9=="?"]<-""
+votingData$V9 <- as.factor(votingData$V2)
+votingData$V10[votingData$V10=="y"]<-"10y"
+votingData$V10[votingData$V10=="n"]<-"10n"
+votingData$V10[votingData$V10=="?"]<-""
+votingData$V10 <- as.factor(votingData$V2)
+votingData$V11[votingData$V11=="y"]<-"11y"
+votingData$V11[votingData$V11=="n"]<-"11n"
+votingData$V11[votingData$V11=="?"]<-""
+votingData$V11 <- as.factor(votingData$V2)
+votingData$V12[votingData$V12=="y"]<-"12y"
+votingData$V12[votingData$V12=="n"]<-"12n"
+votingData$V12[votingData$V12=="?"]<-""
+votingData$V12 <- as.factor(votingData$V12)
+votingData$V13[votingData$V13=="y"]<-"13y"
+votingData$V13[votingData$V13=="n"]<-"13n"
+votingData$V13[votingData$V13=="?"]<-""
+votingData$V13 <- as.factor(votingData$V13)
+votingData$V14[votingData$V14=="y"]<-"14y"
+votingData$V14[votingData$V14=="n"]<-"14n"
+votingData$V14[votingData$V14=="?"]<-""
+votingData$V14 <- as.factor(votingData$V14)
+votingData$V15[votingData$V15=="y"]<-"15y"
+votingData$V15[votingData$V15=="n"]<-"15n"
+votingData$V15[votingData$V15=="?"]<-""
+votingData$V15 <- as.factor(votingData$V15)
+votingData$V16[votingData$V16=="y"]<-"16y"
+votingData$V16[votingData$V16=="n"]<-"16n"
+votingData$V16[votingData$V16=="?"]<-""
+votingData$V16 <- as.factor(votingData$V16)
+votingData$V17[votingData$V17=="y"]<-"17y"
+votingData$V17[votingData$V17=="n"]<-"17n"
+votingData$V17[votingData$V17=="?"]<-""
+votingData$V17 <- as.factor(votingData$V17)
+votingData[1:20,]
+
+library("arules", lib.loc="~/R/win-library/3.2")
+library(datasets)
+#votingBaskets <- read.transactions(file = "C:/Users/maryjoyce/Documents/Towson/Spring 2016/COSC 757/Assignments/Assignment4/UCI data/house-votes-84.data.txt", format="basket", sep = ",")
+# Inspect the dataset
+votingBaskets <- as(votingData,"transactions")
+summary(votingBaskets)
+votingBaskets[1:2,]
+
+# plot the data
+itemFrequencyPlot(votingBaskets, support=0.1, cex.names=0.8)
+# association rules
+rules <- apriori(votingBaskets, parameter = list(support=0.01, confidence=0.6))
+# subset of rules
+rulesDemocrat <- subset(rules, subset=rhs%in%"V1=democrat")
+rulesRepublican <- subset(rules, subset=rhs%in%"V1=republican")
+inspect(sort(rulesDemocrat, by="confidence")[1:5])
+inspect(sort(rulesRepublican, by="confidence")[1:5])
+
+
+
+
+## Decision Tree Classification
+# install.packages("rpart")
+# library(rpart)
+set.seed(1234)
+ind <- sample(2, nrow(balanceScaleData), replace=TRUE,
+              prob=c(0.7,0.3))
+trainData <- balanceScaleData[ind==1,]
+testData <- balanceScaleData[ind==2,]
+# trainData$balanceData <- (trainData$rWeight * trainData$rDistance) - (trainData$lWeight * trainData$lDistance)
+# testData$balanceData <- (testData$rWeight * testData$rDistance) - (testData$lWeight * testData$lDistance)
+# balanceScale_rpart <- rpart(class ~ balanceData, data = trainData)
+balanceScale_rpart <- rpart(class ~ rWeight + rDistance + lWeight + lDistance, data = trainData, method = "class")
+printcp(balanceScale_rpart)
+plotcp(balanceScale_rpart)
+plot(balanceScale_rpart)
+text(balanceScale_rpart, use.n=TRUE)
+balanceScale_pred <- predict(balanceScale_rpart, testData[,-6], type="class")
+#balanceScale_pred <- predict(balanceScale_rpart, newData = testData, type="class")
+balanceScale_pred
+table(balanceScale_pred, testData$class)
+
